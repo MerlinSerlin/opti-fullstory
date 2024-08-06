ebb2d40a94ce2c31566240a15d41bc2eef1517fc
Marshall Serlin <marshallserlin@gmail.com>
2024-08-06 07:56:37 -0700

    hooked up fullstory via server api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import { createFullStoryUser } from "@/utils/fullstory-api-call";
 
 export default function Home() {
+  // Initialize FullStory
+  // initFullStory({ orgId: 'my-org-id' })
+  createFullStoryUser();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
---
+++ src/utils/fullstory-api-call.tsx
@@ -0,0 +1,46 @@
+// Define the FullStory API endpoint
+const url = 'https://api.fullstory.com/v2/users';
+
+// Your FullStory API key
+const apiKey = process.env.FULL_STORY_API_KEY;
+
+console.log(apiKey);
+
+// User data to be created
+const userData = {
+  email: 'user@example.com',
+  display_name: 'John Doe',
+  uid: 'user123',  // Unique identifier for the user
+  properties: {
+    custom_property_1: 'value1',
+    custom_property_2: 'value2'
+  }
+};
+
+// Headers for the request
+const headers = {
+  'Authorization': `Basic ${apiKey}`,
+  'Content-Type': 'application/json'
+};
+
+export const createFullStoryUser = async () => {
+  // Make the POST request
+  fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(userData)
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(`Failed to create user: ${response.status}`);
+    }
+  })
+  .then(data => {
+    console.log('User created successfully:', data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
